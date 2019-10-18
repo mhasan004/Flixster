607caf91b3f65055b994c01e1275960a3f1a768d
Mahmudul Hasan <mhasan0047@gmail.com>
2019-10-18 17:54:16 -0400

    Parsed the first movie JSON object

--- Flixster/app/src/main/java/com/example/flixster/MainActivity.java
+++ Flixster/app/src/main/java/com/example/flixster/MainActivity.java
@@ -8,10 +8,13 @@ import android.util.Log;
 import com.codepath.asynchttpclient.AsyncHttpClient;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import okhttp3.Headers;
 //1) Setting up the Using CodePath Async Http Client
+//2) parsing JSON data
 public class MainActivity extends AppCompatActivity {
     public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"; //1.0) getting the url (passing in the api key as well into the url"
     public static final String TAG = "MainActivity";                            //1.0) Making a tag to easily log data
@@ -28,8 +31,16 @@ public class MainActivity extends AppCompatActivity {
             public void onSuccess(int statusCode, Headers headers, JSON json) {
                 Log.d(TAG, "onSuccess");                                            // just logging the TAG data so we know it worked!
                 JSONObject jsonObject = json.jsonObject;                                 // 1.3) We know the returned result is an object
+                try {
+                    JSONArray results = jsonObject.getJSONArray("results");        // 1.4) String the 'results' key into an array (because the value of the results key is an array). getJSONArray is an unhandled exception. meaning that the results key may not exist, ned to handle it in an catch
+                    Log.i(TAG, "Results" + results.toString());                             //passed the results so im printing what got passed! NOTE: Log.i not Log.e
+                } catch (JSONException e) {
+                    Log.e(TAG, "We hit a JSON exception. Maybe there are no 'results' key in the JSON response?", e);   //failed to get the results
+                    e.printStackTrace();                                                 // 1.4)  getJSONArray() is an unhandled exception. meaning that the results key may not exist, need to handle it in an try catch block
+                }
+
             }
-            ////jh,gnbcngn
+
 
             @Override
             public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
