09b437ffbfbf73a84760e5ef4d6eabc684366863
Mahmudul Hasan <mhasan0047@gmail.com>
2019-10-26 02:32:54 -0400

    Part 2 almost done. need to add videos2

--- app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
@@ -24,11 +24,10 @@ import org.parceler.Parcels;
 import java.util.List;
 
 //  PART 2 HAS 'S' COMMENTS
-public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>     //4.3) After making the ViewHolder class, expand MovieAdapter Class to "extends RecyclerView.Adapter<MovieAdapter.ViewHolder>". the ViewHolder is the one i made here. That's why u make the ViewHolder first!
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>                     //4.3) After making the ViewHolder class, expand MovieAdapter Class to "extends RecyclerView.Adapter<MovieAdapter.ViewHolder>". the ViewHolder is the one i made here. That's why u make the ViewHolder first!
 {
-    Context context;                                                                //4.1.1) This a required member variables need. constructor will give us these
-    List<Movie> movies;                                                             //4.1.2) This is the data. A List of Movie objects. Constructor will define this
-
+    Context context;                                                                                //4.1.1) This a required member variables need. constructor will give us these
+    List<Movie> movies;                                                                             //4.1.2) This is the data. A List of Movie objects. Constructor will define this
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {                             //4.3.1) Will inflate the layout (item_movie.xml) and will return it inside a ViewHolder
@@ -37,56 +36,52 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
         return new ViewHolder(movieView);                                                                       //4.3.3) Wrap the View into a ViewHolder
     }
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {        //4.4.1) Will populate data into the View through the ViewHolder
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {                        //4.4.1) Will populate data into the View through the ViewHolder
         Log.d("MovieAdapter","onBindViewHolder " + position);
-        Movie movie = movies.get(position);                                         //4.4.2) Get the movie at that position. Remember 'movies' is a List of movie objects. Each element has a index. 'position' is simply the index of the List's elements
-        holder.bind(movie);                                                         //4.4.3) Bind the movie data into the ViewHolder. Need to make bind() in ViewHolder class
-
+        Movie movie = movies.get(position);                                                         //4.4.2) Get the movie at that position. Remember 'movies' is a List of movie objects. Each element has a index. 'position' is simply the index of the List's elements
+        holder.bind(movie);                                                                         //4.4.3) Bind the movie data into the ViewHolder. Need to make bind() in ViewHolder class
     }
     @Override
     public int getItemCount() {
         return movies.size();
     }
 
-
-    public MovieAdapter(Context context, List<Movie> movies) {                      //4.1.3E) Constructor defines the two memebr variables
+    public MovieAdapter(Context context, List<Movie> movies) {                                      //4.1.3E) Constructor defines the two memebr variables
         this.context = context;
         this.movies = movies;
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder                         //4.2.1) Define a inner ViewHolder class. Remember: ViewHolder is a representation of the rows in a RecyclerView (which is 'item_movie.xml')
+    public class ViewHolder extends RecyclerView.ViewHolder                                         //4.2.1) Define a inner ViewHolder class. Remember: ViewHolder is a representation of the rows in a RecyclerView (which is 'item_movie.xml')
     {
-        TextView tvTitle;                                                           //4.2.2) Define the view components
+        TextView tvTitle;                                                                           //4.2.2) Define the view components
         TextView tvOverview;
         ImageView ivPoster;
         RelativeLayout rvContainer;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            tvTitle = itemView.findViewById(R.id.tvTitle);                          //4.2.3E) Find the components from the view
+            tvTitle = itemView.findViewById(R.id.tvTitle);                                          //4.2.3E) Find the components from the view
             tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
             rvContainer = itemView.findViewById(R.id.rvContainer);
         }
-        public void bind(final Movie movie) {                                             //4.4.4 or 4.2.4) Connect the single Movie object data to a ViewHolder
+        public void bind(final Movie movie) {                                                       //4.4.4 or 4.2.4) Connect the single Movie object data to a ViewHolder
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
-            Glide.with(context).load(movie.getPosterPath()).into(ivPoster);         //4.4.5E or 4.2.5E) using the Glide library to post an image
+            Glide.with(context).load(movie.getPosterPath()).into(ivPoster);                         //4.4.5E or 4.2.5E) using the Glide library to post an image
 
             rvContainer.setOnClickListener(new View.OnClickListener() {                 //S 1) setting a onclick listener on the RecyclerView so we know when user clicks it
                 @Override
-                public void onClick(View v) {                                               //S 1.1) Want to register a onClickListener on the whole row, not just the title TextView
+                public void onClick(View v) {                                                  //S 1.1) Want to register a onClickListener on the whole row, not just the title TextView
                     Toast.makeText(context, "Showing '" + movie.getTitle()+"'", Toast.LENGTH_SHORT).show();   //S 2.1) Go to a new activity on a tap (create the activity now: project view -> src -> main -> right click -> new>Activity>Empty>"DetailActivity"
                     Intent i = new Intent(context, DetailActivity.class);
-                    //i.putExtra("title", movie.getTitle());              //S 2.2) passing the movie title to the DetailActivity using the key "title"
-                    i.putExtra("movie", Parcels.wrap(movie));                               //***S 2.2) passing each element of a movie item will get tedious. so will pass the activity the entire movie. ISSUE: java has no idea what our custom element Movie is. SOL: Use "Parceler" see the guide. go to Movie.java and add @Parcel and empty constructor: https://guides.codepath.org/android/Using-Parceler
+                    //i.putExtra("title", movie.getTitle());                                        //S 2.2) passing the movie title to the DetailActivity using the key "title"
+                    i.putExtra("movie", Parcels.wrap(movie));                                 //***S 2.2) passing each element of a movie item will get tedious. so will pass the activity the entire movie. ISSUE: java has no idea what our custom element Movie is. SOL: Use "Parceler" see the guide. go to Movie.java and add @Parcel and empty constructor: https://guides.codepath.org/android/Using-Parceler
                     context.startActivity(i);
-
                 }
             });
         }
     }
-
 }
 
 //After 4.4.5E its time to go back to MainActivity.java to connect the views
--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -10,34 +10,34 @@ import java.util.List;
 // Diving the processes. what Main will go and what movies will do. dividing the logics to be clearer
 //3) my makeMovieListFromJsonArray(): make a List of movie objects from a given JSONArray input
 
-@Parcel                                                                     //Part 2 section. added this to use Parcel
+@Parcel                              //Part 2 section. added this to use Parcel
 public class Movie {
-    String posterPath;                                                      //3.0) These are the fields i care about from that JSON object that the website api gives us
+    String posterPath;                                                                                  //3.0) These are the fields i care about from that JSON object that the website api gives us
     String title;
     String overview;
     double rating;
 
-    public Movie() {}                                                       //Part 2 section: empty constructor needed for Parceler library
+    public Movie() {}                //Part 2 section: empty constructor needed for Parceler library
 
-    public Movie(JSONObject jsonObject) throws JSONException{               //3.1) The constructor will make a movie object. If any of these fails, the constructor will throw the JSON Exception
-        posterPath = jsonObject.getString("poster_path");             //3.1) These will have exceptions, instead of putting throw catches, we will add a "throws JSONException" in the contructor def
+    public Movie(JSONObject jsonObject) throws JSONException{                                               //3.1) The constructor will make a movie object. If any of these fails, the constructor will throw the JSON Exception
+        posterPath = jsonObject.getString("poster_path");                                           //3.1) These will have exceptions, instead of putting throw catches, we will add a "throws JSONException" in the contructor def
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
         rating = jsonObject.getDouble("vote_average");
     }
-                                                                            //3.2-3.3) Construct and return a List of 'Movie' object for each item in the array (will pass the'results' JSONArray from Main)
+                                                                                                        //3.2-3.3) Construct and return a List of 'Movie' object for each item in the array (will pass the'results' JSONArray from Main)
     public static List<Movie> makeMovieListFromJsonArray(JSONArray movieJsonArray) throws JSONException
-    {                                                                       //3.2) The 'makeMovieListFromJsonArray()' method will return a list of movies, input: a JSONArray
+    {                                                                                                   //3.2) The 'makeMovieListFromJsonArray()' method will return a list of movies, input: a JSONArray
         List<Movie> movies = new ArrayList<>();
         for (int i=0; i<movieJsonArray.length(); ++i){
-            movies.add(new Movie(movieJsonArray.getJSONObject(i)));         //3.3) will add a movie object to each position in the list
+            movies.add(new Movie(movieJsonArray.getJSONObject(i)));                                     //3.3) will add a movie object to each position in the list
         }
         return movies;
     }
 
     //3.4) Getters
-    public String getPosterPath() {                                             //3.4.1) ISSUE: From the JSON of the site u see that posterPath is only the relative URL (not full URL). See Hints tab of Week1
-        return String.format("https://image.tmdb.org/t/p/w342/%s",posterPath);          //**Hardcoding the path. Should be using the API to find the correct size for attribute: Size of posters are w342, then we are just appending the path to it
+    public String getPosterPath() {                                                                     //3.4.1) ISSUE: From the JSON of the site u see that posterPath is only the relative URL (not full URL). See Hints tab of Week1
+        return String.format("https://image.tmdb.org/t/p/w342/%s",posterPath);                          //**Hardcoding the path. Should be using the API to find the correct size for attribute: Size of posters are w342, then we are just appending the path to it
     }
     public String getTitle() {
         return title;
